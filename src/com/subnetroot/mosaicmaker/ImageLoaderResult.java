9aa1b3b49d8498a0635c4b48b08457ec243abd31
blakeoxx <blakeoxx@netscape.net>
2021-10-27 19:27:53 -0500

    Improved error reporting for image loading

--- src/com/subnetroot/mosaicmaker/ImageLoaderResult.java
+++ src/com/subnetroot/mosaicmaker/ImageLoaderResult.java
@@ -7,18 +7,20 @@ public class ImageLoaderResult
 	public int x;
 	public int y;
 	public BufferedImage image;
+	public Exception loaderError;
 	
 	public ImageLoaderResult()
 	{
 		this.x = 0;
 		this.y = 0;
 		this.image = null;
+		this.loaderError = null;
 	}
 	
-	public ImageLoaderResult(int x, int y, BufferedImage image)
+	public ImageLoaderResult(int x, int y)
 	{
+		this();
 		this.x = x;
 		this.y = y;
-		this.image = image;
 	}
 }
